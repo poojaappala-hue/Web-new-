1f9057669507b2907742a0b365fc1d352dafe881
poojaappala-hue <poojaappala@gmail.com>
2026-02-07 01:05:23 +0530

    Add files via upload

---
+++ Web.js
@@ -0,0 +1,8 @@
+function kk(){
+  document.getElementById("hh").src="kiss (1).gif";
+  document.getElementById("ff").innerHTML="Ummahh!";
+}
+function dd(){
+  document.getElementById("hh").src="pooja.gif";
+  document.getElementById("ff").innerHTML="okahh!";
+}
